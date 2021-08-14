50433fff27531b973c1b21fa364a02d6ba50705d
Bighsueh <40841121@gm.nfu.edu.tw>
2021-08-15 00:41:51 +0800

    試著在mainActivity做一個頁面

--- app/src/main/java/com/example/androidstudiopractice/MainActivity.kt
+++ app/src/main/java/com/example/androidstudiopractice/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.androidstudiopractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn:Button = findViewById(R.id.tap_btn)
+        btn.setOnClickListener{
+            Toast.makeText(this, "Toast!!", Toast.LENGTH_LONG).show()
+        }
     }
 }
+
